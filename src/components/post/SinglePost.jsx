753719a1832843d2cbe46ad653fd179c6ffca7c7
Titas <titasdg@gmail.com>
2019-05-24 14:49:06 +0300

    like

--- src/components/post/SinglePost.jsx
+++ src/components/post/SinglePost.jsx
@@ -12,42 +12,64 @@ import {  Card, CardImg, CardText, CardBody,
         this.state={
            posts:[],
            comments:[],
-           show:true
+           likes:[],
+           show:true,
+           update:false
       
         }
         this.handdleComment=this.handdleComment.bind(this)
+        this.handdleClick = this.handdleClick.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
     componentDidMount() {
         const { id } = this.props.match.params;
        fetch('http://laravel.local/api/post-comments/'+id)
            .then(response => response.json())
            .then(data => {
-               this.setState(prevState =>(
+               const likes=data.post.likes;
+               this.setState(
                    {
+                   likes:likes,
                    posts: data.post,
                    comments:data.comments
-               }
-               ));
+               })
+               
            })
-           this.handleSubmit = this.handleSubmit.bind(this);
+        
    }
+
    componentDidUpdate(){
-       const { id } = this.props.match.params;
-       fetch('http://laravel.local/api/post-comments/'+id)
-           .then(response => response.json())
-           .then(data => {
-               this.setState(prevState =>(
-                   {
-                   
-                   comments:data.comments
-               }
-               ));
-           })
+       
+    const { update } = this.state;
+     
+        if(update==true)
+        {
+            const { id } = this.props.match.params;
+            fetch('http://laravel.local/api/post-comments/'+id)
+                .then(response => response.json())
+                .then(data => {
+                    const likes=data.post.likes;
+                    this.setState(
+                        {
+                       
+                        comments:data.comments,
+                        update:!update
+                    })
+                    
+                })
+        
+                }
       
-   }
+
+        }
+         
+   
+   
    handleSubmit(event) {
     const { id } = this.props.match.params;
     const { show } = this.state;
+    const { update } = this.state;
+    
     event.preventDefault();
     let data = new FormData(event.target);
     data.append('post_id',id);
@@ -55,13 +77,37 @@ import {  Card, CardImg, CardText, CardBody,
     fetch('http://laravel.local/api/add-comment', {
       method: 'POST',
       body: data,
-    });
-    this.setState({show:!show}) 
-}
+    }).then(
+        this.setState(
+            {
+                show:!show,
+                update:true
+                
+                }
+        )
+      
+    )
+    console.log(show);  
+    //this.setState() 
+    }
+
    handdleComment=()=>{
       const { show } = this.state;
+
       this.setState({show:!show}) 
    }
+
+   handdleClick(){
+    const { id } = this.props.match.params;
+    fetch('http://laravel.local/api/add-like/'+id)
+    this.setState(prevState =>({
+            
+       likes:prevState.likes+1
+    
+}));
+   
+    }
+
    
     render(){
         const postComments = this.state.comments.map(comments=>{
@@ -81,7 +127,7 @@ import {  Card, CardImg, CardText, CardBody,
                             <CardSubtitle>Card subtitle</CardSubtitle>
                             <CardText>{this.state.posts.content}</CardText>
                             <Button onClick={this.handdleComment}>Comment</Button>
-                            <Button onClick={this.handdleClick}>Like <span> {this.state.counter}</span></Button>
+                            <Button onClick={this.handdleClick}>Like <span> {this.state.likes}</span></Button>
                                 {this.state.show &&  
                                             <form onSubmit={this.handleSubmit}>
                                                 
